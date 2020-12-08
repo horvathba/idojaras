308d5af324ccf889e977454f29a7bbc382722a64
horvathba <73660375+horvathba@users.noreply.github.com>
2020-12-08 13:47:29 +0100

    3dMarker

--- src/Marker.js
+++ src/Marker.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Marker.css';
 
 const Marker = (props: any) => {
-    const { color, name, id ,  } = props;
+    const { color, name, id } = props;
     return (
-      <div className="marker"
-        style={{ color : color, cursor: 'pointer'}} 
-        title={name}
-      />
+      <div>
+        <div
+          className="pin bounce"
+          style={{ backgroundColor: color, cursor: 'pointer' }}
+          title={name}
+        />
+        <div className="pulse" />
+      </div>
     );
   };
 
